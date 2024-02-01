9ee315f861070254757c3e99c40fc474846577db
wenhaox <42383570+wenhaox@users.noreply.github.com>
2024-02-01 09:58:31 -0500

    Added LED test

--- post_api.py
+++ post_api.py
@@ -19,9 +19,15 @@ def wio_post_to_buzzer(duty_percent,freq):
     url = f"https://cn.wio.seeed.io/v1/node/GenericPWMOutD0/pwm_with_freq/{duty_percent}/{freq}?access_token=07042fa3919ad945a5e9384e1a130789"
     return wio_post(url)
 
+def wio_post_to_led(status):
+    url = f"https://cn.wio.seeed.io/v1/node/GenericDOutD0/onoff/{status}]?access_token=dd6c0019eb310b68c79dea774e6e1972"
+    
+    return wio_post(url)
+
 # status: 0 or 1
 def wio_post_to_relay(status):
-    url = f"https://cn.wio.seeed.io/v1/node/GroveRelayD1/onoff/{status}?access_token=07042fa3919ad945a5e9384e1a130789"
+    url = f"https://cn.wio.seeed.io/v1/node/GroveRelayD1/onoff/{status}?access_token=dd6c0019eb310b68c79dea774e6e1972"
+    
     return wio_post(url)
 
 
---
+++ test/led_test.py
@@ -0,0 +1,6 @@
+from ..post_api import *
+
+
+wio_post_to_led(1)
+wio_post_to_led(0)
+# Check the response
